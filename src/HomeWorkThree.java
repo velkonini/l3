3b1ee07c40f00f4b529f3f5a6838d7d275b92065
velkonini <avelkov07@gmail.com>
2020-05-20 11:08:29 +0300

    Initial commit

---
+++ src/HomeWorkThree.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class HomeWorkThree {
+    static Scanner scanner;
+    static Random random;
+
+    public static void main(String[] args) {
+        scanner = new Scanner(System.in);
+        random = new Random();
+
+        
+
+    }
+}
