0229bf5e04270ce22068e6582ac65fd57f20b55c
velkonini <avelkov07@gmail.com>
2020-05-22 09:51:43 +0300

    HW3

--- src/HomeWorkThree.java
+++ src/HomeWorkThree.java
@@ -1,3 +1,5 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 import java.util.Random;
 import java.util.Scanner;
 
@@ -9,7 +11,35 @@ public class HomeWorkThree {
         scanner = new Scanner(System.in);
         random = new Random();
 
-        
+        guessTheNumber();
 
+    }
+
+
+    static void guessTheNumber() {
+        int x = random.nextInt(10);
+        int userAnswer;
+        int y;
+
+        do {
+            System.out.println("Добро пожаловать в игру \n'Угадай число'!");
+            for (y = 1; y <= 3; y++) {
+                System.out.println("Введите число от 0 до 9");
+                userAnswer = scanner.nextInt();
+                if (userAnswer == x) {
+                    System.out.println("Поздравляю, вы угадали");
+                    break;
+                }
+                if (userAnswer > x) {
+                    System.out.println("Загаданное число меньше");
+                }
+                if (userAnswer < x) {
+                    System.out.println("Загаданное число больше");
+                }
+            }
+            System.out.println("Повторить игру еще раз? 1 – да / 0 – нет");
+
+
+        } while (scanner.nextInt() == 1);
     }
 }
